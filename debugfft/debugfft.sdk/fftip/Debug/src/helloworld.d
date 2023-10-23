46420a1e1f60c6167d47ccf7ad36c60e4dd2182e
Animesh Pareek <108825332+Mr-AniP@users.noreply.github.com>
2023-10-23 05:57:31 +0000

    s3

---
+++ debugfft/debugfft.sdk/fftip/Debug/src/helloworld.d
@@ -0,0 +1,66 @@
+src/helloworld.o src/helloworld.o: ../src/helloworld.c ../src/platform.h \
+ ../src/platform_config.h \
+ ../../fftip_bsp/ps7_cortexa9_0/include/xil_printf.h \
+ ../../fftip_bsp/ps7_cortexa9_0/include/xil_types.h \
+ ../../fftip_bsp/ps7_cortexa9_0/include/xparameters.h \
+ ../../fftip_bsp/ps7_cortexa9_0/include/xparameters_ps.h \
+ ../../fftip_bsp/ps7_cortexa9_0/include/bspconfig.h \
+ ../../fftip_bsp/ps7_cortexa9_0/include/xtime_l.h \
+ ../../fftip_bsp/ps7_cortexa9_0/include/xparameters.h \
+ ../../fftip_bsp/ps7_cortexa9_0/include/xaxidma.h \
+ ../../fftip_bsp/ps7_cortexa9_0/include/xaxidma_bdring.h \
+ ../../fftip_bsp/ps7_cortexa9_0/include/xstatus.h \
+ ../../fftip_bsp/ps7_cortexa9_0/include/xil_assert.h \
+ ../../fftip_bsp/ps7_cortexa9_0/include/xaxidma_bd.h \
+ ../../fftip_bsp/ps7_cortexa9_0/include/xaxidma_hw.h \
+ ../../fftip_bsp/ps7_cortexa9_0/include/xil_io.h \
+ ../../fftip_bsp/ps7_cortexa9_0/include/xil_printf.h \
+ ../../fftip_bsp/ps7_cortexa9_0/include/xpseudo_asm.h \
+ ../../fftip_bsp/ps7_cortexa9_0/include/xreg_cortexa9.h \
+ ../../fftip_bsp/ps7_cortexa9_0/include/xpseudo_asm_gcc.h \
+ ../../fftip_bsp/ps7_cortexa9_0/include/xdebug.h \
+ ../../fftip_bsp/ps7_cortexa9_0/include/xil_cache.h
+
+../src/platform.h:
+
+../src/platform_config.h:
+
+../../fftip_bsp/ps7_cortexa9_0/include/xil_printf.h:
+
+../../fftip_bsp/ps7_cortexa9_0/include/xil_types.h:
+
+../../fftip_bsp/ps7_cortexa9_0/include/xparameters.h:
+
+../../fftip_bsp/ps7_cortexa9_0/include/xparameters_ps.h:
+
+../../fftip_bsp/ps7_cortexa9_0/include/bspconfig.h:
+
+../../fftip_bsp/ps7_cortexa9_0/include/xtime_l.h:
+
+../../fftip_bsp/ps7_cortexa9_0/include/xparameters.h:
+
+../../fftip_bsp/ps7_cortexa9_0/include/xaxidma.h:
+
+../../fftip_bsp/ps7_cortexa9_0/include/xaxidma_bdring.h:
+
+../../fftip_bsp/ps7_cortexa9_0/include/xstatus.h:
+
+../../fftip_bsp/ps7_cortexa9_0/include/xil_assert.h:
+
+../../fftip_bsp/ps7_cortexa9_0/include/xaxidma_bd.h:
+
+../../fftip_bsp/ps7_cortexa9_0/include/xaxidma_hw.h:
+
+../../fftip_bsp/ps7_cortexa9_0/include/xil_io.h:
+
+../../fftip_bsp/ps7_cortexa9_0/include/xil_printf.h:
+
+../../fftip_bsp/ps7_cortexa9_0/include/xpseudo_asm.h:
+
+../../fftip_bsp/ps7_cortexa9_0/include/xreg_cortexa9.h:
+
+../../fftip_bsp/ps7_cortexa9_0/include/xpseudo_asm_gcc.h:
+
+../../fftip_bsp/ps7_cortexa9_0/include/xdebug.h:
+
+../../fftip_bsp/ps7_cortexa9_0/include/xil_cache.h:
---
+++ debugfft/debugfft.sdk/fftip/Debug/src/platform.d
@@ -0,0 +1,16 @@
+src/platform.o src/platform.o: ../src/platform.c \
+ ../../fftip_bsp/ps7_cortexa9_0/include/xparameters.h \
+ ../../fftip_bsp/ps7_cortexa9_0/include/xparameters_ps.h \
+ ../../fftip_bsp/ps7_cortexa9_0/include/xil_cache.h \
+ ../../fftip_bsp/ps7_cortexa9_0/include/xil_types.h \
+ ../src/platform_config.h
+
+../../fftip_bsp/ps7_cortexa9_0/include/xparameters.h:
+
+../../fftip_bsp/ps7_cortexa9_0/include/xparameters_ps.h:
+
+../../fftip_bsp/ps7_cortexa9_0/include/xil_cache.h:
+
+../../fftip_bsp/ps7_cortexa9_0/include/xil_types.h:
+
+../src/platform_config.h:
